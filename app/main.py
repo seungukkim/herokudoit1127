28db6c3951aa62955d0630158f06aa2aacc7ea3e
seungukkim <gogobattle@naver.com>
2022-08-10 12:13:44 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,19 @@
 from flask import Flask, request
+import json
+
+# 메인 로직!! 
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -43,6 +58,37 @@ def showHello():
                     "simpleImage": {
                         "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
                         "altText": "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+
+# 카카오톡 Calculator 계산기 응답
+@app.route('/api/calCulator', methods=['POST'])
+def calCulator():
+    body = request.get_json()
+    print(body)
+    params_df = body['action']['params']
+    print(type(params_df))
+    opt_operator = params_df['operators']
+    number01 = json.loads(params_df['sys_number01'])['amount']
+    number02 = json.loads(params_df['sys_number02'])['amount']
+
+    print(opt_operator, type(opt_operator), number01, type(number01))
+
+    answer_text = str(cals(opt_operator, number01, number02))
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
                     }
                 }
             ]
