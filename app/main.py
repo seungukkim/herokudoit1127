8cdca501b1f0e9254c5d0f9fbb1f7665eee347db
seungukkim <gogobattle@naver.com>
2022-08-11 09:39:00 +0900

    update

--- app/main.py
+++ app/main.py
@@ -105,14 +105,14 @@ def calCulator():
 def whereLive():
     body = request.get_json()
     print(body)
-    print(type(body))#dict
-    print(type(body['action']))#dict
-    print(type(body['action']['detailParams']))#dict
-    print(type(body['action']['detailParams']['sys_number_age']))#dict
-    print(type(body['action']['detailParams']['sys_number_age']['value'])) #str
+    # print(type(body))#dict
+    # print(type(body['action']))#dict
+    # print(type(body['action']['detailParams']))#dict
+    # print(type(body['action']['detailParams']['sys_number_age']))#dict
+    # print(type(body['action']['detailParams']['sys_number_age']['value'])) #str
 
-    print(type(body['action']['params']))# dict
-    print(type(body['action']['params']['sys_number_age'])) #str
+    # print(type(body['action']['params']))# dict
+    # print(type(body['action']['params']['sys_number_age'])) #str
     params_df=body['action']['params']
     temp1=json.loads(params_df['sys_number_age'])
     print(temp1)
