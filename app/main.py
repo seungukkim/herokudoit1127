90f7d9806db45b7162e03a7cee28809a2a75eb32
seungukkim <gogobattle@naver.com>
2022-08-12 16:47:56 +0900

    update

--- app/main.py
+++ app/main.py
@@ -120,18 +120,53 @@ def showHello():
     print(body['userRequest']['utterance'])
 
     responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
+  "version": "2.0",
+  "template": {
+    "outputs": [
+      {
+        "carousel": {
+          "type": "listCard",
+          "items": [
+            {
+              "header": {
+                "title": "샌드위치"
+              },
+              "items": [
                 {
-                    "simpleImage": {
-                        "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-                        "altText": "hello I'm Ryan"
-                    }
+                  "title": "햄치즈",
+                  "description": "4,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_01.jpg"
+                },
+                {
+                  "title": "베이컨 아보카도",
+                  "description": "5,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_02.jpg"
+                },
+                {
+                  "title": "에그 포테이토",
+                  "description": "5,300원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_03.jpg"
+                },
+                {
+                  "title": "갈릭 베이컨 토마토",
+                  "description": "5,800원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_04.jpg"
                 }
-            ]
+              ],
+              "buttons": [
+                {
+                  "label": "더보기",
+                  "action": "message",
+                  "messageText" : "샌드위치 더보기"
+                }
+              ]
+            }           
+          ]
         }
-    }
+      }
+    ]
+  }
+}
 
     return responseBody
 
