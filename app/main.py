8b7900d79d848a5d1864efda37ef97ee98098a1b
seungukkim <gogobattle@naver.com>
2022-08-12 15:37:24 +0900

    update

--- app/main.py
+++ app/main.py
@@ -27,55 +27,80 @@ def sayHello():
     print(body['userRequest']['utterance'])
 
     responseBody = {
-    "version": "2.0",
-    "template": {
+  "version": "2.0",
+  "template": {
     "outputs": [
       {
-        "listCard": {
-          "header": {
-            "title": "챗봇 관리자센터를 소개합니다."
-          },
+        "carousel": {
+          "type": "listCard",
           "items": [
             {
-              "title": "챗봇 관리자센터",
-              "description": "새로운 AI의 내일과 일상의 변화",
-              "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
-              "link": {
-                "web": "https://namu.wiki/w/%EB%9D%BC%EC%9D%B4%EC%96%B8(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
-              }
+              "header": {
+                "title": "샌드위치"
+              },
+              "items": [
+                {
+                  "title": "햄치즈",
+                  "description": "4,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_01.jpg"
+                },
+                {
+                  "title": "베이컨 아보카도",
+                  "description": "5,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_02.jpg"
+                },
+                {
+                  "title": "에그 포테이토",
+                  "description": "5,300원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_03.jpg"
+                },
+                {
+                  "title": "갈릭 베이컨 토마토",
+                  "description": "5,800원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_04.jpg"
+                }
+              ],
+              "buttons": [
+                {
+                  "label": "더보기",
+                  "action": "message",
+                  "messageText" : "샌드위치 더보기"
+                }
+              ]
             },
             {
-              "title": "챗봇 관리자센터",
-              "description": "카카오톡 채널 챗봇 만들기",
-              "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
-              "action": "block",
-              "blockId": "62654c249ac8ed78441532de",
-              "extra": {
-                "key1": "value1",
-                "key2": "value2"
-              }
-            },
-            {
-              "title": "Kakao i Voice Service",
-              "description": "보이스봇 / KVS 제휴 신청하기",
-              "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
-              "action": "message",
-              "messageText": "Kakao i Voice Service",
-              "extra": {
-                "key1": "value1",
-                "key2": "value2"
-              }
-            }
-          ],
-          "buttons": [
-            {
-              "label": "구경가기",
-              "action": "block",
-              "blockId": "62654c249ac8ed78441532de",
-              "extra": {
-                "key1": "value1",
-                "key2": "value2"
-              }
+              "header": {
+                "title": "커피"
+              },
+              "items": [
+                {
+                  "title": "아메리카노",
+                  "description": "1,800원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_05.jpg"
+                },
+                {
+                  "title": "카페라떼",
+                  "description": "2,000원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_06.jpg"
+                },
+                {
+                  "title": "카페모카",
+                  "description": "2,500원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_07.jpg"
+                },
+                {
+                  "title": "소이라떼",
+                  "description": "2,200원",
+                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_08.jpg"
+                }
+              ],
+              "buttons": [
+                {
+                  "label": "더보기",
+                  "action": "message",
+                  "messageText" : "커피 더보기"
+                }
+              ]
             }
           ]
         }
