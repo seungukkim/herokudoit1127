39d9c3113ba722d2a3b76f9a5fbb3aaaff657ee0
seungukkim <gogobattle@naver.com>
2022-08-12 15:34:03 +0900

    update

--- app/main.py
+++ app/main.py
@@ -27,17 +27,62 @@ def sayHello():
     print(body['userRequest']['utterance'])
 
     responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": "안녕 hello I'm Ryan"
-                    }
-                }
-            ]
+    "version": "2.0",
+    "template": {
+    "outputs": [
+      {
+        "listCard": {
+          "header": {
+            "title": "챗봇 관리자센터를 소개합니다."
+          },
+          "items": [
+            {
+              "title": "챗봇 관리자센터",
+              "description": "새로운 AI의 내일과 일상의 변화",
+              "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+              "link": {
+                "web": "https://namu.wiki/w/%EB%9D%BC%EC%9D%B4%EC%96%B8(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
+              }
+            },
+            {
+              "title": "챗봇 관리자센터",
+              "description": "카카오톡 채널 챗봇 만들기",
+              "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
+              "action": "block",
+              "blockId": "62654c249ac8ed78441532de",
+              "extra": {
+                "key1": "value1",
+                "key2": "value2"
+              }
+            },
+            {
+              "title": "Kakao i Voice Service",
+              "description": "보이스봇 / KVS 제휴 신청하기",
+              "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
+              "action": "message",
+              "messageText": "Kakao i Voice Service",
+              "extra": {
+                "key1": "value1",
+                "key2": "value2"
+              }
+            }
+          ],
+          "buttons": [
+            {
+              "label": "구경가기",
+              "action": "block",
+              "blockId": "62654c249ac8ed78441532de",
+              "extra": {
+                "key1": "value1",
+                "key2": "value2"
+              }
+            }
+          ]
         }
-    }
+      }
+    ]
+  }
+}
 
     return responseBody
 
